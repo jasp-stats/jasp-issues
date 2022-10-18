020ce48f713beb42210ce02ecd320ab3e7a55efa
Julius Pfadt <julius.pfadt@gmail.com>
2022-10-18 09:25:17 +0200

    move keywords to its own file

--- .github/scripts/feature_labelAssign.js
+++ .github/scripts/feature_labelAssign.js
@@ -1,64 +1,6 @@
 
 
-module.exports = async function ({github, context}) {
-  // define an array of keyword triples, including the label and 
-  // the maintainer to assign
-  const moduleKeywords = [
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(ANOVA).*\s+\#\#\#/, 
-    "Module: jaspAnova", "JohnnyDoorn"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Audit).*\s+\#\#\#/, 
-    "Module: jaspAudit", "koenderks"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Bain).*\s+\#\#\#/, 
-    "Module: jaspBain", "koenderks"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Bsts).*\s+\#\#\#/, 
-    "Module: jaspBsts", "vandenman"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Circular).*\s+\#\#\#/, 
-    "Module: jaspCircular", "Kucharssim"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Cochrane).*\s+\#\#\#/, 
-    "Module: jaspCochrane", "FBartos"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Descriptives).*\s+\#\#\#/,
-    "Module: jaspDescriptives", "vandenman"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Distributions).*\s+\#\#\#/,
-    "Module: jaspDistributions", "Kucharssim"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Equivalence T-Tests).*\s+\#\#\#/,
-    "Module: jaspEquivalenceTTests", "Jillderon"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Factor).*\s+\#\#\#/,
-    "Module: jaspFactor", "juliuspf"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Frequencies).*\s+\#\#\#/,
-    "Module: jaspFrequencies", "FBartos"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(JAGS).*\s+\#\#\#/, 
-    "Module: jaspJags", "vandenman"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Learn Bayes).*\s+\#\#\#/, 
-    "Module: jaspLearnBayes", "Kucharssim"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Machine Learning).*\s+\#\#\#/,
-    "Module: jaspMachineLearning", "koenderks"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Meta Analysis).*\s+\#\#\#/,
-    "Module: jaspMetaAnalysis", "FBartos"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Mixed Models).*\s+\#\#\#/,
-    "Module: jaspMetaAnalysis", "FBartos"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Network).*\s+\#\#\#/,
-    "Module: jaspNetwork", "vandenman"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Prophet).*\s+\#\#\#/,
-    "Module: jaspProphet", "Kucharssim"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Quality Control).*\s+\#\#\#/,
-    "Module: jaspQualityControl", "vandenman"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Regression).*\s+\#\#\#/,
-    "Module: jaspRegression", "Kucharssim"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Reliability).*\s+\#\#\#/, 
-    "Module: jaspReliability", "juliuspf"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(SEM).*\s+\#\#\#/, 
-    "Module: jaspSem", "LSLindeloo"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Summary Statistics).*\s+\#\#\#/,
-    "Module: jaspSummaryStatistics", "akashrajkn"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(T-Tests).*\s+\#\#\#/,
-    "Module: jaspTTests", "vandenman"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Visual Modeling).*\s+\#\#\#/,
-    "Module: jaspVisualModeling", "dustinfife"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(Other Module).*\s+\#\#\#/,
-    "", "Kucharssim"],
-    [/\#\#\# Is your feature request related to a JASP module\?\s+,*.*(_No response_).*\s+\#\#\#/,
-    "", "AlexanderLyNL"]
-  ];
+module.exports = async function ({github, context}, keywords) {
 
   // get the issue info
   const issue = await github.rest.issues.get({
@@ -81,9 +23,13 @@ module.exports = async function ({github, context}) {
   }
 
   // test if module keyword is in issue body
-  for (const word of moduleKeywords) {
+  const regexModule1 = "\\#\\#\\# Is your feature request related to a JASP module\\?\\s+,*.*("
+  const regexModule2 = ").*\\s+\\#\\#\\#";
 
-    let found = word[0].test(body);
+  for (const words of keywords.modules) {
+    // create the regex to match with, case insensitive
+    var regex = new RegExp(regexModule1 + words[0] + regexModule2, "i");
+    let found = regex.test(body);
 
     // label and assign someone
     if (found) {
@@ -92,7 +38,7 @@ module.exports = async function ({github, context}) {
         owner: context.repo.owner,
         repo: context.repo.repo,
         labels: [
-          word[1]
+          words[1]
         ]
       });
       github.rest.issues.addAssignees({
@@ -100,26 +46,26 @@ module.exports = async function ({github, context}) {
         owner: context.repo.owner,
         repo: context.repo.repo,
         assignees: [
-          word[2]
+          words[2]
         ]
       });
     }
     else { // is there no match but the label is already set? then remove it
-      if (labelNames.includes(word[1])) {
+      if (labelNames.includes(words[1])) {
         github.rest.issues.removeLabel({
           issue_number: context.issue.number,
           owner: context.repo.owner,
           repo: context.repo.repo,
-          name: word[1]
+          name: words[1]
         });
       } // same for assignees
-      if (assigneeNames.includes(word[2])) {
+      if (assigneeNames.includes(words[2])) {
         github.rest.issues.removeAssignees({
           issue_number: context.issue.number,
           owner: context.repo.owner,
           repo: context.repo.repo,
           assignees: [
-            word[2]
+            words[2]
           ]
         });
       }
---
+++ .github/scripts/keywords.js
@@ -0,0 +1,44 @@
+
+
+module.exports = function () {
+  const keywords = {
+    modules: [
+      ["Unrelated", "", "boutinb"],
+      ["ANOVA", "Module: jaspAnova", "JohnnyDoorn"],
+      ["Audit", "Module: jaspAudit", "koenderks"],
+      ["Bain", "Module: jaspBain", "koenderks"],
+      ["Bsts", "Module: jaspBsts", "vandenman"],
+      ["Circular", "Module: jaspCircular", "Kucharssim"],
+      ["Cochrane", "Module: jaspCochrane", "FBartos"],
+      ["Descriptives","Module: jaspDescriptives", "vandenman"],
+      ["Distributions","Module: jaspDistributions", "Kucharssim"],
+      ["Equivalence T-Tests","Module: jaspEquivalenceTTests", "Jillderon"],
+      ["Factor","Module: jaspFactor", "juliuspf"],
+      ["Frequencies","Module: jaspFrequencies", "FBartos"],
+      ["JAGS", "Module: jaspJags", "vandenman"],
+      ["Learn Bayes", "Module: jaspLearnBayes", "Kucharssim"],
+      ["Machine Learning","Module: jaspMachineLearning", "koenderks"],
+      ["Meta Analysis","Module: jaspMetaAnalysis", "FBartos"],
+      ["Mixed Models","Module: jaspMetaAnalysis", "FBartos"],
+      ["Network","Module: jaspNetwork", "vandenman"],
+      ["Prophet","Module: jaspProphet", "Kucharssim"],
+      ["Quality Control","Module: jaspQualityControl", "vandenman"],
+      ["Regression","Module: jaspRegression", "Kucharssim"],
+      ["Reliability", "Module: jaspReliability", "juliuspf"],
+      ["SEM", "Module: jaspSem", "LSLindeloo"],
+      ["Summary Statistics","Module: jaspSummaryStatistics", "akashrajkn"],
+      ["T-Tests","Module: jaspTTests", "vandenman"],
+      ["Visual Modeling","Module: jaspVisualModeling", "dustinfife"],
+      ["Other Module","", "Kucharssim"],
+      ["_No response_", "", "AlexanderLyNL"]
+    ], 
+    oses: [
+      ["macOS Intel", "OS: macOS Intel"],
+      ["macOS Silicon", "OS: macOS Silicon"],
+      ["Windows 10", "OS: Windows 10"], 
+      ["Windows 11", "OS: Windows 11"], 
+      ["Linux|Flatpak|Ubuntu|Debian|Fedora|Arch|Manjaro|PopOs", "OS: Linux / Flatpak"]
+    ]
+  };
+  return keywords;
+}
